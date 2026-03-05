07c615462829ef8cc2152dc5ccb69f184ffc7e44
harryscully <harry.scully@ugogroup.co.uk>
2026-03-05 14:38:59 +0000

    Seed users table with one of each role

---
+++ prisma/seed.ts
@@ -0,0 +1,57 @@
+import bcrypt from "bcryptjs";
+
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+    const salt = await bcrypt.genSalt(10);
+
+    const harry = await prisma.user.upsert({
+        where: { email: "harry.scully@ugogroup.co.uk" },
+        update: {},
+        create: {
+            email: "harry.scully@ugogroup.co.uk",
+            password: await bcrypt.hash("password123", salt),
+            name: "Harry",
+            role: "ADMIN",
+        }
+    });
+    const tom = await prisma.user.upsert({
+        where: { email: "tom@mail.co.uk" },
+        update: {},
+        create: {
+            email: "tom@mail.co.uk",
+            password: await bcrypt.hash("password456", salt),
+            name: "TOM",
+            role: "MANAGER",
+        }
+    });
+    const dick = await prisma.user.upsert({
+        where: { email: "dick@mail.co.uk" },
+        update: {},
+        create: {
+            email: "dick@mail.co.uk",
+            password: await bcrypt.hash("password789", salt),
+            name: "Dick",
+            role: "VIEWER",
+        }
+    }); 
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    await pool.end();
+    process.exit(1);
+  });
