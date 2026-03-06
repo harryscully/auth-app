52322c7a44be4a345a511b0920816850e2398a46
harryscully <harry.scully@ugogroup.co.uk>
2026-03-06 11:58:59 +0000

    Add link to Home on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,18 @@
-
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 import LoginForm from "./LoginForm"
 
 export default function Login() {
     return (
-        <div className="flex w-full min-h-svh items-center justify-center">
-            <LoginForm />
+
+        <div className="flex flex-col w-full min-h-svh p-4">
+            <Button className="self-end" asChild>
+                <Link href="/">Home</Link>
+            </Button>
+            <div className="flex grow items-center justify-center">
+                <LoginForm />
+            </div>
         </div>
+
     )
 }
