6a52c73e3d0bd4bb93214d5e579fa59a0d0127a2
harryscully <harry.scully@ugogroup.co.uk>
2026-03-06 13:12:40 +0000

    Update dashboard

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { auth } from "@/auth"
 import { redirect } from "next/navigation"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import Signout from "@/components/SignOutButton"
 
 export default async function Dashboard() {
     const session = await auth()
@@ -12,17 +11,14 @@ export default async function Dashboard() {
     }
 
     return (
-        <div className="flex flex-col w-full min-h-svh p-4">
-            <Signout />
-            <div className="flex flex-col gap-4 grow items-center justify-center">
-                <h1 className="text-5xl font-mono font-black">Dashboard</h1>
-                <Button asChild>
-                    <Link href="/settings">Settings</Link>
-                </Button>
-                <Button asChild>
-                    <Link href="/profile">Profile</Link>
-                </Button>
-            </div>
-        </div>
+        <>
+            <h1 className="text-5xl font-mono font-black">Dashboard</h1>
+            <Button asChild>
+                <Link href="/settings">Settings</Link>
+            </Button>
+            <Button asChild>
+                <Link href="/profile">Profile</Link>
+            </Button>
+        </>
     )
 }
