57548418ecaf3f1aeb35677b4a67015b1c4919d3
harryscully <harry.scully@ugogroup.co.uk>
2026-03-06 12:46:10 +0000

    Change text-gray to muted-foreground

--- app/unauthorised/page.tsx
+++ app/unauthorised/page.tsx
@@ -5,7 +5,7 @@ export default function Unauthorised() {
     return (
         <div className="flex flex-col w-full min-h-svh items-center justify-center">
             <h1 className="text-4xl font-bold">Access Denied</h1>
-            <p className="text-gray-500 my-4">You don't have permission to view this page.</p>
+            <p className="text-muted-foreground my-4">You don't have permission to view this page.</p>
             <Button variant="outline" asChild>
                 <Link href="/profile">Go back to profile</Link>
             </Button>
