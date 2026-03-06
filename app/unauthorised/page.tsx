0493dccfe0baf277dc2f46b63456d8a0de590821
harryscully <harry.scully@ugogroup.co.uk>
2026-03-06 09:52:04 +0000

    Add link to profile on /unauthorised

--- app/unauthorised/page.tsx
+++ app/unauthorised/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link"
+
 export default function Unauthorised() {
     return (
         <>
             <h1 className="text-4xl font-bold">Access Denied</h1>
             <p>You don't have permission to view this page.</p>
+            <Link href="/profile">Go to profile</Link>
         </>
     )
 }
