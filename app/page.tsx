083ea8dcd0c96a2402f662506a9a4d26b08f5df3
harryscully <harry.scully@ugogroup.co.uk>
2026-03-06 10:59:40 +0000

    Style home page and add link to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <div className="flex flex-col w-full min-h-svh p-4">
+      <Button className="self-end" asChild>
+        <Link href="/login">Login</Link>
+      </Button>
+      <div className="flex grow items-center justify-center">
+        <h1 className="text-5xl font-mono font-black">_auth-app</h1>
+      </div>
+    </div>
   )
 }
